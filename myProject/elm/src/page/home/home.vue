87845627f39a4295847e0e152bcbec90c999330f
yetianyang <yetianyang@cloudmtr.com>
2022-04-15 11:04:22 +0800

    header组件和home组件更改以及加载sassLoder

---
+++ myProject/elm/src/page/home/home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div> 
+
+    <head-top> 
+    <span slot='logo' class="head_logo">ele.me</span>
+    </head-top>
+    <nav class="city_nav">
+      <div class="city_tip">
+        <span>当前定位城市:</span>
+        <span>定位不准时,请在城市列表中选择</span>
+      </div>
+    </nav>
+  </div> 
+
+</template>
+
+
+
+<script>
+import headTop from '../../components/header/header.vue'
+
+export default {
+  setup() {
+    
+  },
+  components:{headTop:headTop}
+  
+}
+</script>
